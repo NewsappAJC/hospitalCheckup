d7200bfb18168c03edd2dbca225c76dc92bc548d
Emily Merwin <emilymerwinajc@gmail.com>
2015-07-14 20:17:45 -0400

    Load data from URL

--- src/assets/js/entities/infection.js
+++ src/assets/js/entities/infection.js
@@ -7,8 +7,31 @@ HospitalCheckup.module("Entities", function(Entities, HospitalCheckup, Backbone,
   });
 
   Entities.InfectionCollection = Backbone.Collection.extend({
-    model: Entities.Infection,
-    comparator: "display_name"
+    url: "/src/assets/data/infections.json",
+    initialize: function(){
+      this.model= Entities.Infection;
+      this.comparator= "display_name" //sort by
+    }
+  });
+
+  var infections;
+  var API = {
+    getInfectionEntities: function(){
+      //TODO make sure the data loads over the network, John used `defer`
+      if(infections === undefined){
+        infections = new Entities.InfectionCollection();
+        infections.fetch({
+          success: function(data){
+            return data
+          }
+        });
+      }
+      return infections;
+    }
+  }
+
+  HospitalCheckup.reqres.setHandler("infection:entities", function(){
+    return API.getInfectionEntities();
   });
 
 });
