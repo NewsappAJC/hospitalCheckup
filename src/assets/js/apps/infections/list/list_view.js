198a40a628ea0bd55a14ffef9335ae360755a83d
Emily Merwin <emilymerwinajc@gmail.com>
2015-07-15 20:03:59 -0400

    Display list as a table
    So I can go ahead and make a release that displays something before
    doing the d3 stuff

--- src/assets/js/apps/infections/list/list_view.js
+++ src/assets/js/apps/infections/list/list_view.js
@@ -1,18 +1,19 @@
 HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Backbone, Marionette, $, _){
 
   List.Infection = Marionette.ItemView.extend({
+    tagName: "tr",
     template: "#infection-list-item",
 
       events: {
-        "click": "highlightName",
+        //"click": "highlightName",
         "click button.js-delete": "deleteClicked",
         "click .js-show": "showClicked"
       },
 
-      highlightName: function(e){
+      /*highlightName: function(e){
         //e.preventDefault();
-        this.$el.toggleClass("panel callout");
-      },
+        this.$el.toggleClass("panel");
+      },*/
       deleteClicked: function(e){
         e.stopPropagation();
         this.trigger("infection:delete", this.model);
@@ -31,10 +32,12 @@ HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Bac
       }
   });
 
-  List.Infections = Marionette.CollectionView.extend({
-    /*tagName: "li",
-    className: "someClass",*/
+  List.Infections = Marionette.CompositeView.extend({
+    tagName: "table",
+    className: "columns",
+    template: "#infection-list",
     childView: List.Infection,
+    childViewContainer: "tbody"
     /*onChildviewInfectionDelete: function(){
       this.$el.fadeOut(1000, function(){
         $(this).fadeIn(1000);
