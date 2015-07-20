533dfaf7e216d90e181feb50110644310e5f3613
Emily Merwin <emilymerwinajc@gmail.com>
2015-07-20 12:08:51 -0400

    Add routing helpers

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,14 @@
 var HospitalCheckup = new Marionette.Application();
 
+HospitalCheckup.navigate = function(route, options){
+  options || (options = {});
+  Backbone.history.navigate(route, options);
+};
+
+HospitalCheckup.getCurrentRoute = function(){
+  return Backbone.history.fragment
+};
+
 HospitalCheckup.on("before:start", function(){
   var RegionContainer = Marionette.LayoutView.extend({
     el: "#app-container",
@@ -16,8 +25,8 @@ HospitalCheckup.on("start", function(){
   if(Backbone.history){
     Backbone.history.start();
 
-    if(Backbone.history.fragment === ""){
-      Backbone.history.navigate("infections");
+    if(this.getCurrentRoute() === ""){
+      this.navigate("infections");
       HospitalCheckup.InfectionsApp.List.Controller.listInfections();
     }
   }
