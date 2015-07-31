27d94096ac1968e718c1d023485211f50c7e78da
Emily Merwin <emilymerwinajc@gmail.com>
2015-07-31 19:34:02 -0400

    Load data from file again

--- src/assets/js/entities/infection.js
+++ src/assets/js/entities/infection.js
@@ -19,67 +19,6 @@ HospitalCheckup.module("Entities", function(Entities, HospitalCheckup, Backbone,
 
   Entities.configureStorage("HospitalCheckup.Entities.InfectionCollection");
 
-  var initializeInfections = function(){
-    var infections = new Entities.InfectionCollection([
-      {
-        "address": {
-          "city": "CUMMING", 
-          "county": "FORSYTH", 
-          "state": "GA", 
-          "street": "1200 NORTHSIDE FORSYTH DRIVE", 
-          "zip": "30041"
-        }, 
-        "display_name": "Northside Hospital Forsyth", 
-        "id": "110005", 
-        "infections": [
-          {
-            "category": "No Different than U.S. National Benchmark", 
-            "days": "3665", 
-            "infection": "cdiff", 
-            "lower": "0.289", 
-            "na": "0", 
-            "note": "", 
-            "observed": "4", 
-            "predicted": "4.398", 
-            "ratio": "0.91", 
-            "upper": "2.194"
-          },
-        ], 
-        "provider_name": "NORTHSIDE HOSPITAL FORSYTH"
-      }, 
-      {
-        "address": {
-          "city": "ATHENS", 
-          "county": "CLARKE", 
-          "state": "GA", 
-          "street": "1230 BAXTER STREET", 
-          "zip": "30606"
-        }, 
-        "display_name": "Saint Mary's Hospital", 
-        "id": "110006", 
-        "infections": [
-          {
-            "category": "Better than the U.S. National Benchmark", 
-            "days": "1962", 
-            "infection": "cdiff", 
-            "lower": "0.009", 
-            "na": "0", 
-            "note": "", 
-            "observed": "1", 
-            "predicted": "5.331", 
-            "ratio": "0.188", 
-            "upper": "0.925"
-          }
-        ], 
-        "provider_name": "ST MARY'S HOSPITAL"
-      }
-    ]);
-    infections.forEach(function(infection){
-      infection.save();
-    });
-    return infections.models;
-  }
-
   var API = {
     getInfectionEntities: function(){
       var infections = new Entities.InfectionCollection();
@@ -93,8 +32,20 @@ HospitalCheckup.module("Entities", function(Entities, HospitalCheckup, Backbone,
       var promise = defer.promise();
       $.when(promise).done(function(fetchedInfections){
         if(fetchedInfections.length === 0){
-          var models = initializeInfections();
-          infections.reset(models);
+          //get models from file
+          $.ajax({
+            dataType: "json",
+            url: "/assets/data/infections.json",
+            type: "GET",
+            success: resetModels
+          });
+
+          function resetModels(models){
+            infections.reset(models);
+            infections.forEach(function(infection){
+              infection.save();
+            });
+          }
         }
       });
       return promise;
