NewsappAJC/hospitalCheckup

commit 1a64a851d4fa5b9d508c8151499e68153d16552d

Fix filter URLs for hospitals
The show_controller is mostly necessary because of URL bookmarking - we
need to request the model because all we know is the ID. When showing a
hospital from the list we already have access to that model.

src/assets/js/apps/infections/infections_app.js
@@ -1,21 +1,23 @@
 HospitalCheckup.module("InfectionsApp", function(InfectionsApp, HospitalCheckup, Backbone, Marionette, $, _){
   InfectionsApp.Router = Marionette.AppRouter.extend({
     appRoutes: {
-      "infections(/)": "listInfections",
-      "infections/:id(/)": "showInfection",
-      "infections(/filter/:criterion)": "listInfections" //dropdown infections filters
+      //"infections(/)": "listInfections",
+      "infections(/:id)(/filter/:criterion)": "listInfections" //hospital selection IDs and dropdown infections filters
     }
   });
 
   var API = {
-    listInfections: function(criterion){
-      InfectionsApp.List.Controller.listInfections(criterion);
-      HospitalCheckup.execute("set:active:header", "infections");
+    listInfections: function(id, criterion){
+      InfectionsApp.List.Controller.listInfections(id,criterion);
+      HospitalCheckup.execute("set:active:header", "infections"); //update navigation toolbar
     },
 
-    showInfection: function(id){
-      InfectionsApp.Show.Controller.showInfection(id);
-      HospitalCheckup.execute("set:active:header", "infections");
+    showHospital: function(id, view){  //received URL with ID parameter
+      InfectionsApp.Show.Controller.showHospital(id, view);
+    },
+
+    changeHospital: function(model, view){ //hospital selected from list
+      InfectionsApp.Show.Controller.changeHospital(model, view);
     }
   };
 
@@ -24,9 +26,13 @@ HospitalCheckup.module("InfectionsApp", function(InfectionsApp, HospitalCheckup,
     API.listInfections();
   });
 
-  HospitalCheckup.on("infection:show", function(id){ //hospital selected from infection list, show hospital detail page
-    HospitalCheckup.navigate("infections/" + id);
-    API.showInfection(id);
+  HospitalCheckup.on("infection:show", function(id, view){ //received URL with ID parameter
+    API.showHospital(id, view);
+  });
+
+  HospitalCheckup.on("infection:change", function(model, view){ //hospital selected from infection list
+    HospitalCheckup.navigate("infections/" + model.get("id"));
+    API.changeHospital(model, view);
   });
 
   HospitalCheckup.on("infections:filter", function(criterion){ //filter menu changed

src/assets/js/apps/infections/list/list_controller.js
@@ -1,7 +1,7 @@
 HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Backbone, Marionette, $, _){
 
   List.Controller = {
-    listInfections: function(criterion){
+    listInfections: function(id, criterion){
       var loadingView = new HospitalCheckup.Common.Views.Loading();
       HospitalCheckup.regions.main.show(loadingView);
 
@@ -23,7 +23,14 @@ HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Bac
         var infectionsListView = new List.Infections({
           collection: filteredInfections
         });
-        var hospitalShowView = new HospitalCheckup.InfectionsApp.Show.Hospital({model: filteredInfections.models[0]});
+        var hospitalShowView = new HospitalCheckup.InfectionsApp.Show.Hospital();
+
+        if(id){ //if we were passed a hospital ID through the URL (i.e. bookmark)
+          HospitalCheckup.trigger("infection:show", id, hospitalShowView);
+        } else { //use the first model in the list as a default
+          hospitalShowView.model = filteredInfections.models[0];
+          hospitalShowView.render();
+        }
 
         infectionsListLayout.on("show", function(){
           infectionsListLayout.menuRegion.show(infectionsMenuView);
@@ -31,10 +38,8 @@ HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Bac
           infectionsListLayout.hospitalRegion.show(hospitalShowView);
         });
 
-        infectionsListView.on("childview:infection:show", function(childView, args){
-          //HospitalCheckup.trigger("infection:show", args.model.get("id"));
-          hospitalShowView.model = args.model;
-          hospitalShowView.render();
+        infectionsListView.on("childview:infection:change", function(childview, args){
+          HospitalCheckup.trigger("infection:change", args.model, hospitalShowView);
         });
 
         infectionsMenuView.on("infections:filter", function(filterCriterion){

src/assets/js/apps/infections/list/list_view.js
@@ -42,7 +42,7 @@ HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Bac
     tagName: "tr",
     template: "#infection-list-item",
     triggers: {
-      "click td .js-show": "infection:show"
+      "click td .js-show": "infection:change"
     }
   });
 

src/assets/js/apps/infections/show/show_controller.js
@@ -1,21 +1,24 @@
 HospitalCheckup.module("InfectionsApp.Show", function(Show, HospitalCheckup, Backbone, Marionette, $, _){
   Show.Controller = {
-    showInfection: function(id){
-      var loadingView = new HospitalCheckup.Common.Views.Loading();
-      HospitalCheckup.regions.main.show(loadingView);
+    showHospital: function(id, view){ //received URL with ID parameter
 
       var fetchingInfection = HospitalCheckup.request("infection:entity", id);
       $.when(fetchingInfection).done(function(infection){
-        var infectionView;
         if(infection !== undefined){
-          infectionView = new Show.Infection({
-            model: infection
-          });
+          view.model = infection;
         } else {
-          infectionView = new Show.MissingHospital();
+           //we should only end up here if someone goes to a URL 
+          //with an ID that none of our hospitals have (that shouldn't happen);
+          view = new Show.MissingHospital();
         }
-        HospitalCheckup.regions.main.show(infectionView);
+
+        view.render();
       });
+    },
+
+    changeHospital: function(model, view){ //hospitalSelected
+      view.model = model;
+      view.render();
     }
   }
 });

commit 1e1432da9997b3c8eb200616be6ee50ae0b50aa4

fix infections filtering from URL bookmark
Broken in 1a64a85 when I stopped triggering infections:filter from the
router

src/assets/js/apps/infections/infections_app.js
@@ -8,7 +8,7 @@ HospitalCheckup.module("InfectionsApp", function(InfectionsApp, HospitalCheckup,
 
   var API = {
     listInfections: function(id, criterion){
-      InfectionsApp.List.Controller.listInfections(id,criterion);
+      InfectionsApp.List.Controller.listInfections(id, criterion);
       HospitalCheckup.execute("set:active:header", "infections"); //update navigation toolbar
     },
 

src/assets/js/apps/infections/list/list_controller.js
@@ -21,7 +21,7 @@ HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Bac
           }
         });
         var infectionsListView = new List.Infections({
-          collection: filteredInfections
+          collection: filteredInfections.filter(criterion)
         });
         var hospitalShowView = new HospitalCheckup.InfectionsApp.Show.Hospital();
 