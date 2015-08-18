3f90f2137743d3e19c07a6ef758d436472cf8494
Emily Merwin <emilymerwinajc@gmail.com>
2015-08-18 16:56:19 -0400

    calculate infections averages
    As soon as I finished this I found out there is a separate state table,
    which makes sense since I’m only looking at Atlanta hospitals so
    wouldn’t be able to calculate the state average from them anyhow

--- src/assets/py/organizeJSON.py
+++ src/assets/py/organizeJSON.py
@@ -9,6 +9,8 @@ src = json.load(f)
 f.close()
 
 tree = []
+totals = { "cauti" : [0,0], "clabsi" : [0,0], "mrsa" : [0,0], "ssicolon" : [0,0], "ssihyst" : [0,0], "cdiff" : [0,0] }
+doneTotals = {}
 
 for node in src:
     hospital = {}
@@ -29,8 +31,14 @@ for node in src:
     #loop through keys looking for the infection substrings and create objects to hold their common properties
     for key in node.keys():
         tmp = key.lower().split("_")
-        if tmp[0] in hospital["infections"]:
-            hospital["infections"][tmp[0]][tmp[1]] = node[key]
+        inf = tmp[0]
+        if inf in hospital["infections"]:
+            hospital["infections"][inf][tmp[1]] = node[key]
+            if tmp[1] == "ratio":
+                val = float(node[key])
+                if(val > 0):
+                    totals[inf][0] += val
+                    totals[inf][1] += 1 #keep track of how many have data so we can average
         # if tmp[0] in keys: #for array lookup
         #if tmp[0] in infections:
             #infections[tmp[0]][tmp[1]] = node[key]
@@ -39,7 +47,14 @@ for node in src:
 
     #we want to loop through an array of infection objects rather than k/v pairs
     #hospital["infections"] = [infections["cauti"], infections["clabsi"], infections["ssicolon"], infections["ssihyst"], infections["cdiff"], infections["mrsa"]]
+
     tree.append(hospital)
+
+for key,val in totals.items():
+    doneTotals[key] = val[0]/val[1]
+    #print "{} = {}".format(key, val)
+
+print doneTotals
 
 f = open( '../data/infections.json', 'w')
 f.write(json.dumps(tree, indent=2, sort_keys=True))
