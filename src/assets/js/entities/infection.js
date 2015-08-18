NewsappAJC/hospitalCheckup

commit f28d369d50f85d609ab6832c4d9c6e707c6e34fb

Wait for URL fetch if no local data
Hospital view was requesting entities from the collection before it was
loaded and failing. Also save it after filterCollection because
Hospital View wasn’t seeming the new property on first rendering (for
some reason it worked when data was in local storage which means it was
ultimately saved)

src/assets/js/entities/infection.js
@@ -35,15 +35,15 @@ HospitalCheckup.module("Entities", function(Entities, HospitalCheckup, Backbone,
   var API = {
     getInfectionEntities: function(){
       var infections = new Entities.InfectionCollection();
-      var defer = $.Deferred();
+      var deferLocal = $.Deferred(); //wait for localStorage data to be fetched
+      var deferServer = $.Deferred(); //we might need to wait for data to be fetched from server
       //check local storage to see if our data is already stored in there
       infections.fetch({
         success: function(data){
-          defer.resolve(data);
+          deferLocal.resolve(data);
         }
       });
-      var promise = defer.promise();
-      $.when(promise).done(function(fetchedInfections){
+      $.when(deferLocal.promise()).done(function(fetchedInfections){
         if(fetchedInfections.length === 0){
           //get models from file. Doing this here instead of by just setting the 
           //collection URL to the file on initialization bc we need to use list 
@@ -54,15 +54,20 @@ HospitalCheckup.module("Entities", function(Entities, HospitalCheckup, Backbone,
             type: "GET",
             success: resetModels
           });
+
+          function resetModels(models){
+            infections.reset(models);
+            infections.forEach(function(infection){
+              infection.save(); //to local storage
+            });
+            deferServer.resolve(infections);
+          }
+        } else {
+          deferServer.resolve(fetchedInfections);
         }
-        function resetModels(models){
-          infections.reset(models);
-          infections.forEach(function(infection){
-            infection.save(); //to local storage
-          });
-        }
+
       });
-      return promise;
+      return deferServer.promise();
     },
 
     getHospitalEntity: function(hospitalId){

src/assets/js/apps/infections/list/list_controller.js
@@ -16,6 +16,7 @@ HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Bac
           filterFunction: function(criterion){
             return function(infection){
               infection.set({measure:criterion || "cdiff"}); //TODO better way to set default before menu renders?
+              infection.save();
               return infection;
             }
           }

commit 3b5732ccebef02b69fa0267662da1d5f74b2f217

Fix Firefox bug introduced in f28d369
I guess it wanted the function declared first?

src/assets/js/entities/infection.js
@@ -34,14 +34,6 @@ HospitalCheckup.module("Entities", function(Entities, HospitalCheckup, Backbone,
           //get models from file. Doing this here instead of by just setting the 
           //collection URL to the file on initialization bc we need to use list 
           //page URL for local storage. If we had a restful API we could use same URL for both
-          $.ajax({
-            dataType: "json",
-            url: "/assets/data/infections.json",
-            //url: "//ajcnewsapps.s3-website-us-east-1.amazonaws.com/2015/staging/hospital-checkup/assets/data/infections.json"
-            type: "GET",
-            success: resetModels
-          });
-
           function resetModels(models){
             infections.reset(models);
             infections.forEach(function(infection){
@@ -49,6 +41,14 @@ HospitalCheckup.module("Entities", function(Entities, HospitalCheckup, Backbone,
             });
             deferServer.resolve(infections);
           }
+
+          $.ajax({
+            dataType: "json",
+            url: "/assets/data/infections.json",
+            //url: "//ajcnewsapps.s3-website-us-east-1.amazonaws.com/2015/staging/hospital-checkup/assets/data/infections.json"
+            type: "GET",
+            success: resetModels
+          });
         } else {
           deferServer.resolve(fetchedInfections);
         }