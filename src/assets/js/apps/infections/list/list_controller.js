a907e4f3ec521bbfddd57207b1c99f06444e5f5e
Emily Merwin <emilymerwinajc@gmail.com>
2015-08-18 21:00:18 -0400

    Get rid of filteredCollection stuff

--- src/assets/js/apps/infections/list/list_controller.js
+++ src/assets/js/apps/infections/list/list_controller.js
@@ -11,19 +11,7 @@ HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Bac
       var infectionsMenuView = new List.Menu();
 
       $.when(fetchingInfections).done(function(infections){
-        var filteredInfections = HospitalCheckup.Entities.FilteredCollection({
-          collection: infections,
-          filterFunction: function(criterion){
-            return function(infection){
-              infection.set({measure:criterion || "cdiff"}); //TODO better way to set default before menu renders?
-              infection.save();
-              return infection;
-            }
-          }
-        });
-
         var infectionsListView = new List.InfectionsChart();
-
         var hospitalShowView = new HospitalCheckup.InfectionsApp.Show.Hospital();
 
         infectionsListLayout.on("show", function(){
@@ -40,7 +28,7 @@ HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Bac
         infectionsListView.on("show", function(){
           List.infectionsChartView = new HospitalCheckup.Common.Chart.BarBase({ //adding it to List module so we can target it later
             el: "#infections-chart",
-            collection: filteredInfections.filter(criterion),
+            collection: infections,
             base_height: 700,
             bar_padding: 4,
             margin: {left: 190, right: 30, bottom: 20, top: 25},
@@ -51,7 +39,7 @@ HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Bac
           if(id){ //if we were passed a hospital ID through the URL (i.e. bookmark)
             HospitalCheckup.trigger("hospital:show", id, hospitalShowView);
           } else { //use the first model in the list as a default
-            hospitalShowView.model = filteredInfections.models[0];
+            hospitalShowView.model = infections.models[0];
             //hospitalShowView.render();
           }
         });
