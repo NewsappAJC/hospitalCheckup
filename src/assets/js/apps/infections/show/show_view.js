ee3a7b6eca6ce028f0f460d5366af3eb380dca23
Emily Merwin <emilymerwinajc@gmail.com>
2015-08-26 16:01:16 -0400

    fixup don't recreate child views

--- src/assets/js/apps/infections/show/show_view.js
+++ src/assets/js/apps/infections/show/show_view.js
@@ -14,8 +14,8 @@ HospitalCheckup.module("InfectionsApp.Show", function(Show, HospitalCheckup, Bac
     },
     onDomRefresh: function(){ //render won't work because we need our parent template to render the #hospital-charts div
       //add hospital charts to Show.Hospital's child #hospital-charts
-      this.chartCollection.reset(this.get_hospital_models(this.model));
       this.charts.setElement(this.$('#hospital-charts')).render(); //which child element to render view in
+      this.chartCollection.reset(this.get_hospital_models(this.model), {silent: true}); //silent bc it was initializing view twice
     },
     get_hospital_models: function(model){
       var dataArr = [];
