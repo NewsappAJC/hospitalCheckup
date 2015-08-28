NewsappAJC/hospitalCheckup

commit 8ba67b6cbdaf129dec3ad7fa61201623a092983d

Create entity of infection names and use it to build menu
We needed this in order to label the infection detail views for the
hospitals (next up)

src/assets/js/apps/infections/list/list_view.js
@@ -15,10 +15,12 @@ HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Bac
 
   List.Menu = Marionette.ItemView.extend({
     template: "#infections-menu-template",
+    tagName: "select",
+    id: "js-infections-filter-criterion",
 
     events: {
       //TODO I read something about IE8 not really firing change event http://www.bentedder.com/backbone-change-events-on-select-menus/
-      "change #js-infections-filter-criterion": "filterInfections" //looks like this broke the menu's display updating
+      "change": "filterInfections" //looks like this broke the menu's display updating
     },
 
     ui: {

src/assets/js/apps/infections/list/list_controller.js
@@ -9,7 +9,7 @@ HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Bac
 
       var infectionsListLayout = new List.Layout(),
       hospitalLayout = new HospitalCheckup.InfectionsApp.Show.HospitalLayout(),
-      infectionsMenuView = new List.Menu(),
+      infectionsMenuView = new List.Menu({collection: HospitalCheckup.Entities.InfectionLabels}),
       infectionsListView = new List.InfectionsChart(),
       hospitalShowView = new HospitalCheckup.InfectionsApp.Show.Hospital(),
       hospitalChartsView = new HospitalCheckup.InfectionsApp.Show.HospitalChartList({collection: new Backbone.Collection()});

src/assets/js/entities/infection.js
@@ -20,6 +20,15 @@ HospitalCheckup.module("Entities", function(Entities, HospitalCheckup, Backbone,
   });
   Entities.configureStorage("HospitalCheckup.Entities.InfectionCollection");
 
+  Entities.InfectionLabels = new Backbone.Collection([
+    {label: "Clostridium difficile (C.diff)", key: "cdiff"},
+    {label: "Methicillin-resistant staphylococcus sureus (MRSA)", key: "mrsa"},
+    {label: "Catheter-associated urinary tract infections", key: "cauti"},
+    {label: "Central line-associated blood stream infections", key: "clabsi"},
+    {label: "Surgical Site Infection from colon surgery", key: "ssicolon"},
+    {label: "Surgical Site Infection from abdominal hysterectomy", key: "ssihyst"}
+  ]);
+
   var API = {
     getInfectionEntities: function(){
       Entities.averages = new Entities.StateAverages(); //attach to Entities so chart can get at it

src/index.html
@@ -111,7 +111,7 @@
   </script>
 
   <script type="text/template" id="hospital-chart-template">
-    <h4><%= measure %></h4>
+    <h4><%= label %></h4>
     <% if (category === "Not Available") {%>
       <p><%= category %></p>
     <% } else { %>
@@ -151,14 +151,9 @@
   </script>
   
   <script type="text/template" id="infections-menu-template">
-    <select id="js-infections-filter-criterion">
-      <option value="cdiff">Clostridium difficile (C.diff)</option>
-      <option value="mrsa">Methicillin-resistant staphylococcus sureus (MRSA)</option>
-      <option value="cauti">Catheter-associated urinary tract infections</option>
-      <option value="clabsi">Central line-associated blood stream infections</option>
-      <option value="ssicolon">Surgical Site Infection from colon surgery</option>
-      <option value="ssihyst">Surgical Site Infection from abdominal hysterectomy</option>
-    </select>
+      <% _.each(items, function(item){ %>
+        <option value="<%= item.key %>"><%= item.label %></option>
+      <% }) %>
   </script>
   <!-- END INFECTIONS
   ================================================== -->

commit d17c1831f46cd8fd4eb606689eacff0f95acf0e1

Fix UI bug introduced in 8ba67b6
The `<select>` element was no longer a child so couldn’t use it as a UI
  element

src/assets/js/apps/infections/list/list_view.js
@@ -43,10 +43,6 @@ HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Bac
       "change": "filterList" //looks like this broke the menu's display updating
     },
 
-    ui: {
-      criterion: "#js-filter-criterion"
-    },
-
     filterList: function(e){
       e.preventDefault();
       var criterion = $(e.currentTarget).val();
@@ -54,10 +50,8 @@ HospitalCheckup.module("InfectionsApp.List", function(List, HospitalCheckup, Bac
     },
 
     onSetFilterCriterion: function(criterion){
-      var el = this.ui.criterion; //in case no filter has been selected yet we'll need the default
-      el.val(criterion || el.val());
+      this.$el.val(criterion || this.$el.val());
     }
-
   });
 
   List.InfectionsChart = Marionette.ItemView.extend({