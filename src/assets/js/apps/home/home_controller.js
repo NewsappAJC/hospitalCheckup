dc95f6d11fa802dbfb5c909d41311688e380146f
Emily Merwin <emilymerwinajc@gmail.com>
2015-09-09 20:50:47 -0400

    Fix homepage gap at the bottom

--- src/assets/js/apps/home/home_controller.js
+++ src/assets/js/apps/home/home_controller.js
@@ -9,6 +9,15 @@ HospitalCheckup.module("HomeApp", function(HomeApp, HospitalCheckup, Backbone, M
       });
 
       HospitalCheckup.regions.main.show(homeView);
+
+      // if home page is too short for height of browser window, make footer the length of the rest of the window
+      var windowHeight = $(window).height(),
+      $footer = $('#homepage-bottom');
+      var gap = windowHeight - $footer.position().top - parseInt($footer.css("borderTopWidth"));
+
+      if (gap > $footer.height()){
+      	$footer.height(gap);
+      }
     }
   };
 });
